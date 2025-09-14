b71b51af697122c0b44e84b685f1daa77d3fd001
Gaurav <Gauravdeshmukh425@gmail.com>
2025-09-14 23:47:27 +0530

    Learnt Some Basic programs

---
+++ pythonBasic3.py
@@ -0,0 +1,8 @@
+def addition(a,b):
+    return a+b
+
+# Now we can use this function to add two numbers
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+result = addition(a,b)
+print("The Sum of", a, "and", b, "is: ", result)
---
+++ pythonBasic4.py
@@ -0,0 +1,7 @@
+a = 5
+b = 4
+def random(a, b):
+    return (a+4,b+5)
+
+m = random(a, b)
+print(m)
