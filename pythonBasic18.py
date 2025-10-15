af0ba78ab35e860f850c79b1f666885beb1cd306
Gaurav <Gauravdeshmukh425@gmail.com>
2025-10-15 20:59:42 +0530

    learned about some new concept of numbers in python

---
+++ pythonBasic18.py
@@ -0,0 +1,7 @@
+number1 = 19.54
+number2 = int(number1)
+
+print(number2)
+
+number3 = float(number2)
+print(number3)
---
+++ pythonBasic19.py
@@ -0,0 +1,5 @@
+import random
+#this random library is used to generate numbers randomly
+#it can generate random float numbers between 0 and 1
+
+print(random.randrange(1, 20))
